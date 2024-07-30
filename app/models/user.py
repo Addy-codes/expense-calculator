c481d7369a1eee1dedf727b5cd08805e8601347c
Addy-codes <adeeb9839@gmail.com>
2024-07-31 01:54:19 +0530

    fix: remove unique contact number

--- app/models/user.py
+++ app/models/user.py
@@ -16,4 +16,4 @@ class User(Base):
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
     email = Column(String, unique=True, index=True, nullable=False)
     name = Column(String, index=True, nullable=False)
-    mobile_number = Column(String, unique=True, index=True, nullable=False)
+    mobile_number = Column(String, index=True, nullable=False)
